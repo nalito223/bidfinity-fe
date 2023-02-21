4734e5e39bfdad19de804746d5e78a78ffab4479
Tim T <tim.email.personal@gmail.com>
2023-02-20 15:46:57 -1000

    Create app, install router, install typescript

---
+++ bidfinity-fe/src/Components/App/App.tsx
@@ -0,0 +1,16 @@
+
+import { NavLink, Route, Routes } from "react-router-dom"
+import React from "react"
+import './App.css'
+import Nav from '../Nav/Nav'
+
+const App: React.FC = () => {
+  return (
+    <main className="App">
+      <h1>Test</h1>
+      <Nav />
+    </main>
+  );
+}
+
+export default App;
---
+++ bidfinity-fe/src/Components/Nav/Nav.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./Nav.css"
+
+
+const Nav: React.FC = () => {
+  return (
+    <nav>
+      nav
+    </nav>
+  )
+}
+
+export default Nav
